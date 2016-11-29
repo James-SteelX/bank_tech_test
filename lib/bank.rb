4266ede8c110e33e09af9621d052ce232aba8204
James <James.Steel@me.com>
2016-11-29 11:06:34 +0000

    working, tests passing

--- lib/bank.rb
+++ lib/bank.rb
@@ -2,6 +2,7 @@ class Bank
 
   def initialize(balance = 0)
     @current_balance = balance.to_f
+    @history = []
   end
 
   def display_balance
@@ -10,14 +11,32 @@ class Bank
 
   def deposit(amount)
    @current_balance += amount.to_f
+   add_statement_details(credit: amount.to_f)
   end
 
   def withdraw(amount)
    if amount <= @current_balance
      @current_balance -= amount.to_f
+     add_statement_details(debit: amount.to_f)
    else
-     'Insufficent funds'
+     raise "Insufficent funds"
    end
+  end
+
+  def show_statement
+   puts "date       ||  credit   || debit   || balance "
+    @history.each do |entry|
+     puts "#{entry[:date]} || #{entry[:credit]}      || #{entry[:debit]}    || #{entry[:balance]}"
+    end
+  end
+
+  private
+
+  def add_statement_details(credit: "----", debit: "----" )
+    @history.push({date: Time.now.strftime("%d/%m/%Y"),
+                   credit: credit,
+                   debit: debit,
+                   balance: @current_balance })
   end
 
 end
